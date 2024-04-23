6d1df0b7cbd0271972a819649f86ef7cc72bc49e
Лужняк Александр <Lyzhniak2005@yandex.ru>
2024-04-23 16:45:38 +0300

    Part of functional

--- SimpleTGBot/TelegramBot.cs
+++ SimpleTGBot/TelegramBot.cs
@@ -28,7 +28,7 @@ public class TelegramBot
     /// <param name="message"></param>
     public async void WriteToLog(string message)
     {
-        using (var sw = new StreamWriter("global_log.txt"))
+        using (var sw = new StreamWriter("global_log.txt", true))
         {
             sw.WriteLine($"[{DateTime.Now}] {message} ");
         }
@@ -188,7 +188,7 @@ public class TelegramBot
                 WriteToLog($"User: {chatId}  Message: {messageText}");
                 Message message1 = await botClient.SendStickerAsync(
                chatId: chatId,
-               sticker: new Telegram.Bot.Types.InputFileUrl("https://raw.githubusercontent.com/MMCS-FIIT/cs211a-bonus2-olekasndrsvl/main/SimpleTGBot/Resources/sticker_glad.webp"),
+               sticker: new Telegram.Bot.Types.InputFileUrl("https://raw.githubusercontent.com/MMCS-FIIT/cs211a-bonus2-olekasndrsvl/main/SimpleTGBot/Resources/sticker_interested.webp"),
                replyMarkup: replyKeyboardMarkup1,
                cancellationToken: cancellationToken);
 
@@ -224,11 +224,48 @@ public class TelegramBot
 
 
                 break;
-                
+
+            case "Коротко о главном":
+
+                _ = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "<b>Коротко о главном</b> \r\n\r\nДанный текст содержит краткий обзор особенностей PascalABC.NET. \r\n\r\nPascalABC.NET – легковесная и мощная среда разработки программ с подробной справочной системой, средствами подсказки по коду, автоформатированием, встроенным отладчиком и встроенным дизайнером форм. Интегрированная среда разработки PascalABC.NET ориентирована на создание проектов малой и средней сложности, а также на обучение современному программированию. \r\nPascalABC.NET – мощный и современный язык программирования. По предоставляемым возможностям он превосходит язык Delphi и содержит практически все возможности языка C#. \r\nPascalABC.NET опирается на платформу Microsoft .NET - её языковые возможности и библиотеки, что делает его гибким, эффективным, постоянно развивающимся. Кроме того, можно легко сочетать библиотеки, разработанные на PascalABC.NET и других .NET-языках. \r\nКомпилятор PascalABC.NET генерирует код, выполняющийся так же быстро, как и код на C#, и несколько медленнее, чем код на C++ и Delphi. \r\nPascalABC.NET является представителем линейки современных языков Паскаль вместе с Delphi и Oxygene. \r\nМнение, что язык Паскаль устарел и утрачивает свои позиции, основано на представлении о старом Паскале и старых средах программирования (например, Free Pascal с его несовременной консольной оболочкой). К сожалению, масса отечественной учебной литературы с упорством, достойным лучшего применения, ориентируется на отживший Turbo Pascal с древней консольной оболочкой, бедной графической библиотекой и устаревшими средствами объектно-ориентированного программирования, развивая у обучающихся стойкое отвращение к языку Паскаль вообще. \r\nPascalABC.NET расширен современными языковыми возможностями для легкого, компактного и понятного програмирования. \r\nPascalABC.NET – достаточно зрелая среда. Ее прототип – учебная система Pascal ABC – появилась в 2002 году. PascalABC.NET – развивающаяся среда. Ведутся разработки новых языковых возможностей, новых библиотек. \r\nДалее приводится ряд программ с короткими комментариями, раскрывающих возможности и особенности языка PascalABC.NET.  \r\n\r\nТексты программ располагаются в рабочей папке (по умолчанию C:\\PABCWork.NET) в подпапке Samples\\!MainFeatures.\r\n\r\nДля запуска программ данное окно должно быть открыто через пункт меню \"Помощь/Коротко о главном\" так, чтобы оно не полностью закрывало окно оболочки PascalABC.NET.\r\n",
+                replyMarkup: replyKeyboardMarkup,
+                parseMode: ParseMode.Html,
+                cancellationToken: cancellationToken);
+                WriteToLog($"User: Bot  Message: Содержание \"коротко о главном\" ");
+
+               Message message2 = await botClient.SendTextMessageAsync(
+               chatId: chatId,
+               text: "Ознакомиться более подробно со справкой по языку PascalABC.NET Вы всегда можете по ссылке:\n https://pascalabc.net/downloads/pabcnethelp/index.htm ",
+               replyMarkup: replyKeyboardMarkup,
+               parseMode: ParseMode.Html,
+               cancellationToken: cancellationToken);
+                WriteToLog($"User: Bot  Message: Reminding message  ");
 
 
 
 
+                break;
+
+            case "Отличия языка PascalABC.NET от Delphi":
+                _ = await botClient.SendTextMessageAsync(
+               chatId: chatId,
+               text: "<b>Отличия языка PascalABC.NET от Delphi</b>\r\n<i>Добавлено</i>\r\nОперации += -= для событий .NET и для процедурных переменных. \r\nОперации += -= *= для целых и  += -= *= /= для вещественных. \r\nОперация += для строк. \r\nПодпрограммы с переменным числом параметров. \r\nОперация new для вызова конструктора (ident := new type_name(params);). \r\nОперация new для создания динамического массива. \r\nОперация typeof . \r\nИспользование uses для подключения пространств имен .NET (реализовано в Delphi Prism). \r\nВид доступа internal (наряду с public, private, protected). \r\nИнициализация переменных: var a: integer := 1; \r\nИнициализация переменных: var a := 1; \r\nОбъявление локальных переменных в блоке. \r\nОбъявление параметра цикла в заголовке цикла: for var i := 1 to 10 do, foreach var x in a do. \r\nОператор lock, обеспечивающий синхронизацию потоков. \r\nМетоды в записях. \r\nИнициализаторы полей в классах и записях. \r\nОбобщенные классы (generics). \r\nРеализованы типизированные файлы (в отличие от Delphi Prism, где они убраны). \r\nУпрощенный синтаксис модулей. \r\nОписание методов внутри интерфейса класса или записи. \r\nРеализация записью интерфейса. \r\nМетоды расширения. \r\nЛямбда-выражения. \r\n<i>Изменено</i>\r\nТолько сокращенное вычисление логических выражений. \r\nДругой синтаксис foreach. \r\nИнтерфейсы interface в стиле .NET. \r\nДругой синтаксис перегрузки операций. \r\nСтатические методы классов вместо классовых методов. Отсутствие типа TClass. \r\nДеструкторы оставлены лишь для совместимости и не выполняют никаких действий. \r\nТип object - синоним System.Object. \r\nТип exception - синоним System.Exception. \r\nИндексация string с 1, директива переключения на индексацию с 0. \r\nПроцедура Write выводит любые типы. \r\nСтруктурная эквивалентность типов для процедурных переменных, динамических массивов, типизированных указателей и множеств (в Delphi Object Pascal - именная эквивалентность типов за исключением открытых массивов). \r\nМножества на базе произвольных типов (set of string). \r\nЗапрет использования указателей на управляемую память. \r\nПроцедурные переменные (делегаты) вместо procedure of object. \r\nС бестиповыми файлами file можно работать с помощью процедур Read, Write. \r\nМассивы массивов отличаются по типу от двумерных массивов (в частности, записи a[i][j] и a[i,j] неэквивалентны). \r\nПерегрузка выполняется без ключевого слова overload. \r\nВсе конструкторы имеют имя Create. \r\nАвтоматическое управление памятью с помощью сборщика мусора (за исключением указателей на неуправляемую память). \r\n",
+               replyMarkup: replyKeyboardMarkup,
+               parseMode: ParseMode.Html,
+               cancellationToken: cancellationToken);
+                WriteToLog($"User: Bot  Message: Содержание \"Отличия языка PascalABC.NET от Delphi\" ");
+               Message messager = await botClient.SendTextMessageAsync(
+               chatId: chatId,
+               text: "Ознакомиться более подробно со справкой по языку PascalABC.NET Вы всегда можете по ссылке:\n https://pascalabc.net/downloads/pabcnethelp/index.htm ",
+               replyMarkup: replyKeyboardMarkup,
+               parseMode: ParseMode.Html,
+               cancellationToken: cancellationToken);
+                WriteToLog($"User: Bot  Message: Reminding message  ");
+
+
+                break;
             default:
 
                 // Отправляем обратно то же сообщение, что и получили
